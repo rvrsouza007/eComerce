62e31c15b1c9eb5cf10b43e333f2f9b84118c694
Rhuan Victor <rvsouzaramos07@gmail.com>
2022-12-14 18:38:31 -0300

    finalizada a Api-teste

--- src/app.js
+++ src/app.js
@@ -2,50 +2,20 @@
 // import {createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa} from "./controller/Pessoa.js";
 
 import express from "express";
+import fs from 'fs';
+import https from 'https';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 import router from "./router.js";
 app.use(router);
 
-// app.get("/", (req, res) => {
-//     res.send("ola mundo");
-// });
-
-// app.get("/pessoas", async function (req, res) {
-//     let pessoas = await selectPessoas();
-//     res.json(pessoas);
-// });
-
-// app.get("/pessoa", async function (req, res) {
-//     let pessoa = await selectPessoa(req.body.id);
-//     res.json(pessoa);
-// });
-
-// app.post("/pessoa", (req, res) => {
-//     insertPessoa(req.body);
-//     res.json({
-//         statusCode: 200,
-//     });
-// });
-
-// app.put("/pessoa", (req, res) => {
-//     if (req.body && !req.body.id) {
-//         res.json({
-//             statusCode: "400",
-//             msg: "vc precisa informar um id",
-//         });
-//     } else {
-//         updatePessoa(req.body);
-//         res.json({
-//             statusCode: 300,
-//         });
-//     }
-// });
-
-// app.delete("/pessoa", async function (req, res) {
-//     let pessoa = await deletePessoa(req.body.id);
-//     res.json(pessoa);
-// });
-
 app.listen(3000, () => console.log("Api rodando."));
+
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3001, ()=> console.log("rodando em https"))
