4aa9bbefa49128cb80bc37b05411b9d4bcc7490b
Rhuan Victor <rvsouzaramos07@gmail.com>
2022-12-14 12:53:28 -0300

    simplificando a Api com router

--- src/app.js
+++ src/app.js
@@ -1,51 +1,51 @@
 // import {openDb} from './configDB.js'
-import { createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa } from './controller/Pessoa.js';
+// import {createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa} from "./controller/Pessoa.js";
 
-import express from 'express';
+import express from "express";
 const app = express();
 app.use(express.json());
 
-createTable();
+import router from "./router.js";
+app.use(router);
 
-app.get('/', (req, res)=>{
-    res.send("ola mundo");
-})
+// app.get("/", (req, res) => {
+//     res.send("ola mundo");
+// });
 
-app.get('/pessoas', async function(req, res){
-    let pessoas = await selectPessoas();
-    res.json(pessoas);
-})
+// app.get("/pessoas", async function (req, res) {
+//     let pessoas = await selectPessoas();
+//     res.json(pessoas);
+// });
 
-app.get('/pessoa', async function(req, res){
-    let pessoa = await selectPessoa(req.body.id);
-    res.json(pessoa);
-})
+// app.get("/pessoa", async function (req, res) {
+//     let pessoa = await selectPessoa(req.body.id);
+//     res.json(pessoa);
+// });
 
-app.post('/pessoa', (req, res)=>{
-    insertPessoa(req.body);
-    res.json({
-        "statusCode": 200
-    })
-})
+// app.post("/pessoa", (req, res) => {
+//     insertPessoa(req.body);
+//     res.json({
+//         statusCode: 200,
+//     });
+// });
 
-app.put('/pessoa', (req, res)=>{
+// app.put("/pessoa", (req, res) => {
+//     if (req.body && !req.body.id) {
+//         res.json({
+//             statusCode: "400",
+//             msg: "vc precisa informar um id",
+//         });
+//     } else {
+//         updatePessoa(req.body);
+//         res.json({
+//             statusCode: 300,
+//         });
+//     }
+// });
 
-    if(req.body && !req.body.id){
-        res.json({
-            "statusCode": "400",
-            "msg":"vc precisa informar um id"
-        })
-    }else{
-        updatePessoa(req.body);
-        res.json({
-            "statusCode": 200
-        })
-    }
-});
+// app.delete("/pessoa", async function (req, res) {
+//     let pessoa = await deletePessoa(req.body.id);
+//     res.json(pessoa);
+// });
 
-app.delete('/pessoa', async function(req, res){
-    let pessoa = await deletePessoa(req.body.id);
-    res.json(pessoa);
-})
-
-app.listen(3000, ()=>console.log("Api rodando."));
+app.listen(3000, () => console.log("Api rodando."));
